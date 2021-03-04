8f56ec9debe7a3e3fea84a751f85ad3cd5f47cba
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-03-03 22:15:03 -0600

    started seed file and table creation

---
+++ scripts/tables.js
@@ -0,0 +1,27 @@
+const {Client} = require('pg')
+const client = new Client({
+    database: 'algos',
+})
+await client.connect()
+
+
+const createProblemsTable = `CREATE TABLE problems(
+    id INTEGER PRIMARY KEY,
+    title varChar(255) NOT NULL,
+    level varChar(22) NOT NULL,
+    frequency INTEGER NOT NULL,
+    acceptance INTEGER NOT NULL
+);`
+
+await client.query(createProblemsTable)
+
+const insertStatement = 'INSERT INTO problems (id, title,level,frequency,acceptance) VALUES ($1, $2, $3, $4, $5);'
+for(let i = 0; i < problemArr.length; i++){
+    try{
+        await client.query(insertStatement, problemArr[i])
+    } catch (err) {
+        console.error(err.stack)
+    }
+}
+
+client.end()
